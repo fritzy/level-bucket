8d731db7ce96cc57752a0dfb5c95ef42be93b2e9
Nathan Fritz <fritzy@netflint.net>
2014-06-23 23:12:11 -0700

    removed logs

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ function LevelBucket(db, opts) {
     db.registerKeyPreProcessor(function (key, opts) {
         opts = opts || {};
         var newkey = (opts.bucket || db._configBucket.default) + db._configBucket.sep + key;
-        console.log("KPP", key, "->", newkey);
         return newkey;
     });
     
@@ -28,36 +27,3 @@ function LevelBucket(db, opts) {
 }
 
 module.exports = LevelBucket;
-
-/*
-var util = require('util');
-var concat = require('concat-stream');
-var levelup = require('./node_modules/levelup');
-var tst = levelup('./testdbforme', {valueEncoding: 'json'});
-tst = LevelBucket(tst);
-
-tst.put('testkey', {test: 'no'}, {bucket: 'weee'}, function (err) {
-});
-
-tst.batch()
-    .put('hi', {derp: 1}, {bucket: 'ham'})
-    .put('wat', {derp: 2}, {bucket: 'ham'})
-    .write(function (err) {
-        tst.get('hi', {bucket: 'ham'}, function (err, value) {
-            console.log(1);
-            console.log("%j", arguments);
-        });
-        tst.createReadStream({bucket: 'ham', start: '!', end: '~'}).pipe(concat(function () {
-            console.log(2);
-            console.log(util.inspect(arguments, {depth: 10}));
-        }));
-        tst.createKeyStream({bucket: 'ham', start: '!', end: '~'}).pipe(concat(function () {
-            console.log(3);
-            console.log(arguments);
-        }));
-        tst.createValueStream({bucket: 'ham', start: '!', end: '~'}).pipe(concat(function () {
-            console.log(4);
-            console.log(arguments);
-        }));
-    });
-    */
